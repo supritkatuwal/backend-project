3278b2f1814da66def9802f4b87efc43d6696d77
supritkatuwal <supritkatuwal91@gmail.com>
2026-02-12 18:14:27 +0545

    adding databse

---
+++ readme.md
@@ -0,0 +1,2 @@
+erase.io
+- [Model link] (https://app.eraser.io/workspace/N2PBaG4xSsX0BOVcfDVp)
---
+++ src/index.js
@@ -0,0 +1,74 @@
+import dotenv from "dotenv"
+import { DB_name } from "./constants.js"
+import connectDB from "./db/connectDB.js"
+
+dotenv.config({
+    path: "./env"
+})
+
+connectDB()
+
+
+
+
+
+
+
+
+// const connectDB = async ()=>{
+//     try {
+//         await mongoose.connect(`${process.env.MONGODB_URI}/{DB_name}}`)
+//         console.log(`\n mongoDB connected !! DB host : ${connectionInstance}`)
+//     } catch (error) {
+//         console.error("mongo connection error", error)
+//         process.exit(1)
+//     }
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// one approach using await to connect db
+// import express from "express";
+// const app= express();
+
+// (async () => {
+//     try {
+//        await mongoose.connect(`${process.env.MONGODB_URI}/{DB_name}`)
+//         app.on("error",()=>{
+//             console.log("error", error)
+//             throw error
+//         })
+
+// app.listen(process.env.PORT,()=>{
+//     console.log(`app is listening on ${process.env.PORT}`)
+// })
+
+//     } catch (error) {
+//         console.error("error", error);
+//         throw err
+//     }
+// })()
